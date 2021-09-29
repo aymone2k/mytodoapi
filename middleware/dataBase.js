14c4f20e06994adb96a15374a3b2a04684265fda
2kalm <aymone.koissy@gmail.com>
2021-09-29 08:53:28 +0200

    config database

--- middleware/dataBase.js
+++ middleware/dataBase.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose')
 
 async function config() {
     await mongoose.connect( process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Attention Connexion à MongoDB échouée !'));
+    /* .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Attention Connexion à MongoDB échouée !')); */
+    .then(async mongoose=>{
+        try{
+            console.log('Connexion à MongoDB réussie !');
+           
+        }
+        finally{
+            mongoose.connection.close()
+        }
+    })
 }
 
-module.exports.config = config;
+module.exports.config = config; 
