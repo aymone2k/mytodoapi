f3a416f312468ad4d3c6cf9fc7ce5d16d5ccb3ab
2kalm <aymone.koissy@gmail.com>
2021-09-29 08:58:58 +0200

    test db

--- middleware/dataBase.js
+++ middleware/dataBase.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose')
 
-async function config() {
+async function config(arg1, arg2, arg3) {
     await mongoose.connect( process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    /* .then(() => console.log('Connexion à MongoDB réussie !'))
+    /* .then(() => console.log())
     .catch(() => console.log('Attention Connexion à MongoDB échouée !')); */
-    .then(async mongoose=>{
+    .then(async mongoose => {
         try{
             console.log('Connexion à MongoDB réussie !');
-           
+            await command.execute(client, message, args);
         }
         finally{
-            mongoose.connection.close()
+            mongoose.connection.close();
         }
-    })
+    });
 }
 
 module.exports.config = config; 
